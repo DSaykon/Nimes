cfe17fbd8466055d841e05984b8b2d61e759e005
DSaykon <saykon@mail.ru>
2022-02-14 19:52:05 +1000

    Practise 2022.02.14

--- main.js
+++ main.js
@@ -3,3 +3,16 @@ const userButton = document.querySelector('modal-link');
 const loginInput = modalWindow.querySelector('#login');
 const loginPassword = modalWindow.querySelector('#password');
 const modelButton = modalWindow.querySelector('.form__btn');
+const errorLogin = modalWindow.querySelector('.error-login');
+const errorPassword = modalWindow.querySelector('.error-password');
+
+
+function toggleModel() {
+    modalWindow.classList.toggle('hidden');
+    loginInput.classList.remove('invalid');
+    loginPassword.classList.remove('invalid');
+    errorLogin.classList.add('hidden');
+    errorPassword.classList.add('hidden');
+    loginInput.value = '';
+    loginPassword.value = '';
+}
