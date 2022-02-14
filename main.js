9752e753856a7c2c2eb529a640d7496620d3ae56
DSaykon <saykon@mail.ru>
2022-02-14 18:34:09 +1000

    Add  07.02.2022

---
+++ main.js
@@ -0,0 +1,5 @@
+const modalWindow = document.querySelector('.form');
+const userButton = document.querySelector('modal-link');
+const loginInput = modalWindow.querySelector('#login');
+const loginPassword = modalWindow.querySelector('#password');
+const modelButton = modalWindow.querySelector('.form__btn');
